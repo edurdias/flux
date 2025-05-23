464d8d82716fe44d68f289f5109a5c0d323bab1b
Eduardo Dias <edurdias@gmail.com>
2025-05-23 19:10:50 -0400

    Reorder imports in __init__.py to prevent circular imports

--- flux/__init__.py
+++ flux/__init__.py
@@ -5,12 +5,14 @@ from flux.catalogs import *
 from flux.context_managers import *
 from flux.decorators import task
 from flux.decorators import workflow
-from flux.domain import ExecutionContext
 from flux.domain.events import *
+from flux.domain.execution_context import ExecutionContext
 from flux.encoders import *
 from flux.output_storage import *
 from flux.secret_managers import *
 from flux.tasks import *
+# Import ExecutionContext first to avoid circular imports
+# Then import the rest of the modules
 
 __all__ = [
     "task",
