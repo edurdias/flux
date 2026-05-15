5084cad2670f6bdb5e66a6da595751bac2ac8f67
Eduardo Dias <edurdias@gmail.com>
2026-05-14 22:05:34 -0400

    feat(agents): first-class agent session tracking + silent-swallow fixes (#68)

--- tests/flux/console/test_executions.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from flux.console.screens.executions import ExecutionsView
-
-
-class TestExecutionsView:
-    def test_creates(self):
-        view = ExecutionsView()
-        assert view is not None
